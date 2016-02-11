e7ba135226e105014403b4f1ec223a67a1b734ef
Jiayi Lu <jl6583@login-0-0.local>
2016-02-11 10:07:09 -0500

    results generated

--- A1/src/train.py
+++ A1/src/train.py
@@ -10,6 +10,7 @@ import argparse
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--poolSize',type=int, nargs='*',default=[2])
 arg_parser.add_argument('--filtSize',type=int, nargs='*',default=[5])
+arg_parser.add_argument('--dropoutProb',type=float, nargs='*', default=[0,0.5])
 args = arg_parser.parse_args()
 args_dict = vars(args)
 print args
@@ -24,4 +25,5 @@ for key, val_list in args_dict.iteritems():
         print "Hyper parameter value = ", val
         print "Now executing command:\n", "th doall.lua -size small -save {} -nepochs 1 -{} {}".format(key+"="+str(val),key,val)
 #Note that nepochs are hard coded as 1 here!!! We might want to use it as another cmd arg in the future
-        os.system("th doall.lua -size small -save {} -nepochs 1 -{} {}".format("results_"+key+"="+str(val),key,val))
+        #os.system("th doall.lua -size small -augment -save {} -nepochs 10 -{} {} -sub".format("results_aug"+key+"="+str(val),key,val))
+        os.system("th doall.lua -size small -save {} -nepochs 10 -{} {} -sub".format("results_"+key+"="+str(val),key,val))
