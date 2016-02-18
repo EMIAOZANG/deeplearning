6d0d4bdc9a53bc8e7a5b19f148534c79595e3e8a
Charles Guthrie <charles.d.guthrie@gmail.com>
2016-02-18 18:29:58 -0500

    added A2

---
+++ A2/src/data_parse.lua
@@ -0,0 +1,31 @@
+--[[
+This file is used to parse the STL-10 dataset into one Tensor.
+By Jake Zhao @NYU
+For DS-GA-1008 2016 Spring
+--]]
+
+
+numSamples = 8000
+numChannels = 3
+height = 96
+width = 96
+
+function parseData(d)
+   local t = torch.ByteTensor(numSamples, numChannels, height, width)
+   local idx = 1
+   for i = 1, #d do
+      local this_d = d[i]
+      for j = 1, #this_d do
+    t[idx]:copy(this_d[j])
+    idx = idx + 1
+      end
+   end
+   assert(idx == numSamples+1)
+   return t
+end
+
+
+-- Usage example, parsing the test data
+test_data = torch.load('test.t7b')
+parse_test_data = parseData(test_data.data)
+--torch.save('parsed_test.t7b', parse_test_data)
