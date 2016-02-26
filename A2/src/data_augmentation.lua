698bea9cf7ebb468f74e060ab3c44db0b8e0dc2e
EMIAOZANG <christopher.w.lu@gmail.com>
2016-02-26 16:22:16 -0500

    added predict.lua

--- A2/src/data_augmentation.lua
+++ A2/src/data_augmentation.lua
@@ -1,28 +1,30 @@
-require 'nn'
-require 'class'
-require 'cunn'
 require 'image'
+require 'nn'
 
-do --data augmentation module and class definition
-   local BatchRotate, parent = class('nn.BatchRotate', 'nn.Module')
+do
+   local BatchGetPatch, parent = torch.class('nn.BatchGetPatch', 'nn.Module')
 
-   function BatchRotate:__init()
+   function BatchGetPatch:__init()
       parent.__init(self)
       self.train = true
    end
 
-   function BatchRotate:updateOutput(input)
+   function BatchGetPatch:updateOutput(input, patchSize)
       if self.train then
-         local bs = input:size(1) --get the size of 1st dimension (number of images)
-         local rotate_mask = torch.randperm(bs):le(bs/2)
-         for i=1,input:size(1) do
-            if rotate_mask[i] == 1 then image.rotate(input[i], input[i],0.35*2*(math.random()-0.5)) end --performs local rotation, copy rotation probably better?
+         local bs = input:size(1)
+         for i=1,bs do
+            local x1_max = input[i]:size(3)-2*patchSize --patchSize is the patch size
+            local y1_max = input[i]:size(2)-2*patchSize
+            --get a random point from [1,x1_max] and [1,y1_max] as the topleft starting point of cropping
+            local x1 = math.random(x1_max)
+            local y1 = math.random(y1_max)
+            local output = torch.Tensor(bs,3,2*patchSize)
+
+            image.crop(input[i], input[i], x1,y1, x1+2*patchSize, y1+2*patchSize) --Crop each image in current batch
          end
-         self.output:set(input)
       end
+
+      self.output:set(output)
+      return self.output
    end
-
 end
-
-
-
